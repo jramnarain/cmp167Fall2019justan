b000aef02f25f8eb520f8e5abd88a1bf0ce01d58
jramnarain <55101832+jramnarain@users.noreply.github.com>
2019-09-11 12:31:29 -0400

    Add files via upload

---
+++ week3.java
@@ -0,0 +1,30 @@
+package week3;
+
+public class week3 {
+
+	public static void main(String[] args) {
+//		System.out.println(78+98);// 176
+//		System.out.println('a'+'b'); // 195
+//		System.out.println('a'-'b'); // -1
+//		System.out.println('0'+'0'); // 96
+//		System.out.println('a'+1); //98
+//		System.out.println('a');// a
+		
+		
+		int num = 100;
+		double dollar = -200;
+		String name1 ="Justan";
+		String name2 = "Brian";
+	
+		
+				
+		System.out.printf("I have %d  %n", num);// i have 18 cars
+		System.out.printf("Ronaldy has %n $%.2f in her pocket", dollar);
+		
+		System.out.printf("%n %s has $%d more than %s",name1, num, name2);  
+	
+		
+
+	}
+
+}
