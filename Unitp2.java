9769b18849e701981116c42294b7f1ff9a8488ae
jramnarain <55101832+jramnarain@users.noreply.github.com>
2019-09-18 12:16:45 -0400

    Add files via upload

---
+++ Unitp2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Unitp2 {
+
+	public static void main(String[] args) {
+		int a, b;
+		
+		Scanner in = new Scanner(System.in);
+				
+		System.out.println("This program checks if a is divisible by b");
+		System.out.println("Enter a: ");
+		a= in.nextInt();
+		System.out.print("Enter b: ");
+		b= in.nextInt();
+		
+		if( a%b==0) {
+			System.out.printf("%d is divisible by %d ", a, b);
+		}else { 
+			System.out.printf("%d is not divisible by %d ", a, b);
+		}
+	}
+
+}
